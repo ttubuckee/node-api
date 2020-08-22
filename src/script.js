425d740df7c2254fc8d653d9c421393cf9d32da3
dlehdanakf <dlehdanakf@konkuk.ac.kr>
2020-08-22 16:12:10 +0900

    비콘 스탬프 인증 route 추가

--- src/script.js
+++ src/script.js
@@ -1,20 +1,5 @@
 /* global process */
 /* global global */
-/*
-	이 서버의 역할
-
-	Prefix
-
-	- /beacon
-		/info/:id  -> 해당 id를 가진 비콘의 정보를 JSON 형식으로 보내준다.
-		/image/:id -> 해당 id를 가진 비콘의 이미지 정보를 보내준다.
-	- /route
-		/info/:id  -> 해당 id를 가진 경로의 정보를 JSON 형식으로 보내준다.
-		/list/:id  -> 해당 id를 가진 경로의 비콘 리스트를 JSON 형식으로 보내준다.
-
-
- */
-
 require('dotenv').config();
 import express from "express";
 import path from "path";
